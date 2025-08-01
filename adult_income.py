49b9d7c50d425e040999ffd2c32ac1df5333d76e
Jothi Arumugam <130521379+joaaru@users.noreply.github.com>
2025-07-31 22:03:08 -0700

    Update adult_income.py

--- adult_income.py
+++ adult_income.py
@@ -5,6 +5,23 @@ import joblib
 from sklearn.ensemble import IsolationForest
 import json
 
+education_dict = {'HS-grad' : 9,
+'Some-college' : 10,
+'Bachelors' : 13,
+'Masters' : 14,
+'Assoc-voc' : 11,
+'11th' : 7,
+'Assoc-acdm' : 12,
+'10th' : 6,
+'7th-8th' : 4,
+'Prof-school' : 15,
+'9th' : 5,
+'12th' : 8,
+'Doctorate' : 16,
+'5th-6th' : 3,
+'1st-4th' : 2,
+'Preschool' : 1}
+
 # Page title
 st.title('Income prediction')
 st.markdown('This model predicts if the income of a person will be >50K or <=50K based on the input parameters. This model ' \
@@ -13,13 +30,13 @@ st.markdown('This model predicts if the income of a person will be >50K or <=50K
 
 age = st.sidebar.slider("Age",min_value=17,max_value=90,value=30)
 
-workclass = st.sidebar.selectbox("Workclass",['state-gov', 'self-emp-not-inc', 'private', 'federal-gov',
+workclass = st.sidebar.selectbox("Workclass",['state-gov', 'self-emp-not-inc', 'private', 
        'local-gov', 'self-emp-inc', 'without-pay', 'never-worked'])
 
-education = st.sidebar.selectbox("Education",['bachelors', 'middle/high school', 'masters', 'assoc-acdm',
-       'assoc-voc', 'doctorate', 'prof-school', 'elementary school','preschool'])
+education = st.sidebar.selectbox("Education",['Preschool','1st-4th','5th-6th','7th-8th','9th','10th','11th','12th','HS-grad',
+                                                  'Some-college','Assoc-voc','Assoc-acdm','Bachelors','Masters','Prof-school','Doctorate']
 
-maritalstatus = st.sidebar.selectbox("Marital status",['never-married', 'married-civ-spouse', 'divorced',
+maritalstatus = st.sidebar.selectbox("Marital status",['never-married', 'married-civ-spouse', 
        'married-spouse-absent', 'separated', 'married-af-spouse','widowed'])
 
 occupation = st.sidebar.selectbox("Occupation",['exec-managerial', 'handlers-cleaners','prof-specialty', 
@@ -53,12 +70,13 @@ df['age'] = age
 df['capital-gain'] = capitalgain
 df['capital-loss'] = capitalloss
 df['hours-per-week'] = hours
+df['education-num'] = education_dict[education]
 if sex=='Male':
     df['sex'] = 1
 
 # categorical values
-col_prefix = ['education_','workclass_','occupation_','marital-status_','relationship_','race_','native-country_']
-col_values = [education,workclass,occupation,maritalstatus,relationship,race,nativecountry]
+col_prefix = ['workclass_','occupation_','marital-status_','relationship_','race_','native-country_']
+col_values = [workclass,occupation,maritalstatus,relationship,race,nativecountry]
 
 cols_to_update = [x + y for x, y in zip(col_prefix, col_values)]
 
@@ -68,15 +86,19 @@ for col in cols_to_update:
 # df is updated with input values. Start the prediction
 # Load the data
 basic_models = {
-'LR_model': joblib.load("model_LR.pkl"),
-'SVM_model': joblib.load("model_SVM.pkl"),
-'XGB_model': joblib.load("model_XGB.pkl")
+'LR_model': joblib.load("s_model_LR.pkl"),
+'RF_model': joblib.load("s_model_RF.pkl"),
+'SVM_model': joblib.load("s_model_SVM.pkl"),
+'XGB_model': joblib.load("s_model_XGB.pkl")
+'KNN_model': joblib.load("s_model_KNN.pkl")
 }
 
 tuned_models = {
-    'Tuned_LR': joblib.load("tuned_LR.pkl"),
-    'Tuned_SVM': joblib.load("tuned_SVM.pkl"),
-    'Tuned_XGB': joblib.load("tuned_XGB.pkl")
+    'Tuned_LR': joblib.load("s_tuned_LR.pkl"),
+    'Tuned_RF': joblib.load("s_tuned_RF.pkl"),
+    'Tuned_SVM': joblib.load("s_tuned_SVM.pkl"),
+    'Tuned_XGB': joblib.load("s_tuned_XGB.pkl")
+    'Tuned_KNN': joblib.load("tuned_KNN.pkl")
 }
 if st.button("Predict"):
     model = tuned_models['Tuned_XGB']
